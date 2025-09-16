a9027998493f21c94060bc6f2ae07f5b4dc5e9a3
Md saiduzzaman saad <142034434+saiduzzamansaad@users.noreply.github.com>
2025-09-16 18:56:03 +0600

    code update

--- day1.js
+++ day1.js
@@ -1,3 +1,13 @@
 console.log("Hello World from JavaScript!");
 console.log("hello world");
 console.log("Hello world");
+
+var country = 'Bangladesh';
+console.log(country);
+
+var country = 'India';
+console.log(country);
+
+
+var country  = 'Pakistan';
+console.log(country);
